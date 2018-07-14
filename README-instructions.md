2a1fa6b049518afb76f4cd097414278c709a08c5
git push -u origin <rhong24@gmail.com>
2018-07-13 23:33:46 -0700

    commit

---
+++ README-instructions.md
@@ -0,0 +1,27 @@
+Instructions:
+- npm install: involves included express, adds googleapis (https://www.npmjs.com/package/googleapis), node-cache (https://www.npmjs.com/package/node-cache), and nodemon (https://www.npmjs.com/package/nodemon)
+- After installation, cd into directory and run 'npm start'
+- On going to localhost:3007/, depending on whether the user has made an API call already, the user will either be redirected to /connect or /calendar-events
+- localhost:3007/connect will be connected to Google oAuth client, which will eventually redirect to /connect/callback to gain Authorization. Getting Authorization will set credentials to access token and refresh token based on CLIENT_ID and CLIENT_SECRET which are stored within .env file for security purposes.
+- After gaining access, a res.send will be sent to explicitly ask the user to navigate to /calendar-events
+- Navigating to /calendar-events will do the initial API call to the user's google calendar's events, which has the following parameters as default:
+{
+  calendarId: 'primary',
+  maxResults: 5,
+  singleEvents: true,
+  orderBy: 'startTime',
+  timeMin: req.query.startDate || new Date().toISOString(),
+}
+
+Note: if a startDate query is not given, the API call will assume the user wants the starting time of events to be listed based on the time the API is called. This can affect the API being requested as this will lead it to think that the user is wanting a new start time including the new amount of seconds, which is supported within the timeMin parameters.
+
+- During the API call, certain factors will be taken into consideration:
+  i.
+    Have the events from an API call have already been stored? For the first time it is called, there are no stored events so it knows to go ahead and continue with the API call, res.send the results, and then store these events within the cache as an object called storedEvents. In addition, the parameters in which this initial API call was made will be stored within the cache in an object called storedQueryvalues.
+  ii.
+    If there are events stored but the query endDate does not match that of the endDate value within storedQueryvalues or does not exist, an API call will be requested again with the criterion of the stored value of startDate. After doing the new API request, the new values will be stored with storedQueryvalues.
+  iii.
+    If there are events stored but the query startDate does not match that of the startDate value within storedQueryvalues or does not exist, an API call will be requested again with the query startDate or default of new Date().toISOString(). After doing the new API request, the new values will be stored with storedQueryvalues.
+
+Note:
+- by default, when the user redirects to /calendar-events within some browsers e.g. Chrome, a # will be appended to the end making it effectively a redirect to /calendar-events# . This seems to be a design choice of the google API call to prevent having sensitive auth data from one app accessible to another.
--- app.js
+++ app.js
@@ -7,12 +7,11 @@ const NodeCache = require( "node-cache" );
 const OAuth2 = google.auth.OAuth2;
 const myCache = new NodeCache();
 
-let savedTokens
 
 const googleAuth = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  process.env.DOMAIN + '/connect/callback'
+  '235823170428-he0p8ultkvaupgoehvmoss1l97hko78v.apps.googleusercontent.com',
+  'FZmBuJmaEnBT3IF299Ndgwy2',
+  'http://localhost:3007/connect/callback'
 )
 
 const GOOGLE_SCOPE = [
@@ -36,57 +35,72 @@ app.get('/connect/callback', function(req, res) {
     } else {
       googleAuth.setCredentials(tokens)
       // console.log('tokens: ', tokens)
-      res.redirect('../../calendar-events')
+      //res.redirect('../../calendar-events')
+    res.send('Authenticated; navigate to /calendar-events now!')
     }
   })
 })
 
 app.get('/calendar-events', function (req, res2) {
-  let stringifiedevents
-  myCache.get('storedEvents', function(err, value) {
+  //console.log('line 45 ', req.query.startDate, req.query.endDate)
+  myCache.mget(['storedQueryvalues', 'storedEvents'], (err, value) => {
     if (!err) {
-      if (value === undefined) {
-        const calendar = google.calendar({ version: 'v3', auth: googleAuth })
-        let criterion = {
-          calendarId: 'primary',
-          timeMin: req.query.startDate || (new Date()).toISOString(),
-          maxResults: 5,
-          singleEvents: true,
-          orderBy: 'startTime',
-        }
+      //console.log('line 48 is being read: ', value)
+      if (value.storedEvents === undefined || (req.query.startDate && req.query.startDate !== value.storedQueryvalues.startDate) ||
+      (req.query.endDate && req.query.endDate !== value.storedQueryvalues.endDate) || (value.storedQueryvalues.endDate && !req.query.endDate) || !req.query.startDate) {
+        const grabEventList = new Promise(resolve => {
+          const calendar = google.calendar({ version: 'v3', auth: googleAuth })
+          let criterion = { // object passed along into line 83
+            calendarId: 'primary',
+            maxResults: 5,
+            singleEvents: true,
+            orderBy: 'startTime',
+            timeMin: req.query.startDate || new Date().toISOString(),
+          }
 
-        if (req.query.endDate) {
-          criterion['timeMax'] = req.query.endDate
-        }
-
-        calendar.events.list({ ...criterion }, (err, res) => {
-          if (err) return console.log('The API returned an error: ' + err);
-          const events = res.items;
-          let resultObj = {
+          let resultObj = { // JSON to be sent eventually with added information that is not necessary in line 83
             status: 200,
             query: {
-              startDate: req.query.startDate,
-              endDate: req.query.endDate || null
-            },
-            results: {
-              ...events
+              startDate: criterion.timeMin,
             }
           }
-          stringifiedevents = resultObj
-          myCache.set('storedEvents', resultObj, function(err, success) {
-            if (!err && success) {
-              console.log('Events have been saved in cache!')
-            } else {
-              console.log(err)
-            }
+
+          if (req.query.endDate) { // accounting for when to add an optional endDate, etc. to JSONs resultObj & criterion
+            criterion['timeMax'] = req.query.endDate
+            resultObj['query']['endDate'] = req.query.endDate
+            myCache.set('storedQueryvalues', { startDate: req.query.startDate, endDate: req.query.endDate })
+          } else {
+            myCache.set('storedQueryvalues', { startDate: criterion.timeMin })
+          }
+
+          calendar.events.list({ ...criterion }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const events = res.items;
+            resultObj['events'] = events
+
+            myCache.set('storedEvents', resultObj, function(err, success) {
+              if (!err && success) {
+                console.log('Events have been saved in cache!')
+                resolve(resultObj)
+              } else {
+                console.log(err)
+              }
+            })
           })
-          res2.send(stringifiedevents)
         })
+        grabEventList
+        //.then(console.log('testtest 1231232!!!!'))
+        //.then(myCache.mget(['storedEvents', 'storedQueryvalues'], (err, value) => { console.log(value) }))
+        .then(result => res2.send(result))
       } else {
-        res2.send(value)
+        console.log('No need to do another api call, retrieving stored events')
+        res2.send(value.storedEvents)
       }
+    } else {
+      console.log(err)
     }
   })
+
 })
 
 app.get('/', function (req, res) {
